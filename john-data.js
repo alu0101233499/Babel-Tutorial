4613a6590da86f1474dc80e6f1190fb8d114a68e
Lorenzo Gabriel Pérez González <alu0101233499@ull.edu.es>
2022-04-02 00:29:22 +0100

    Primer commit

---
+++ john-data.js
@@ -0,0 +1,50 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Programación de Aplicaciones Interactivas
+  * Babel Tutorial
+  *
+  * @author Lorenzo Gabriel Pérez González
+  * @since April 1 2022
+  * @module john-data.js
+  * @desc This program takes a JSON file with the data for a man called John
+  *       Johnson. Once it takes the file, it shows the information with the
+  *       correct format.
+  * @see {@link https://github.com/alu0101233499/Babel-Tutorial.git}
+  */
+
+'use strict';
+
+import * as fs from 'fs';
+import * as process from 'process';
+
+const myArgs = process.argv.slice(2);
+const FILE = fs.readFileSync('./json/John.json', 'utf8');
+export const JSON_OBJECT = JSON.parse(FILE);
+
+const ERROR_MESSAGE = 'Bad input. ' +
+  'The correct way to execute the program is: ' +
+  'node john-data.js';
+
+/**
+ * This function shows the json's data.
+ * @param {Object} jsonObject Object with John Johnson's information.
+ * @returns 
+ */
+export const showData = function(jsonObject) {
+  let data = '';
+  for (const property in jsonObject) {
+    data += `${property}: ${jsonObject[property]}` + '\n';
+  }
+  return data;
+};
+
+try {
+  if (myArgs.length > 0) {
+    throw new Error();
+  }
+  console.log(showData(JSON_OBJECT));
+} catch (error) {
+  console.log(ERROR_MESSAGE);
+}
---
+++ john-data.spec.js
@@ -0,0 +1,26 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ * Babel Tutorial
+ *
+ * @author Lorenzo Gabriel Pérez González
+ * @since April 1 2022
+ * @desc john-data.js tests.
+ *       This file has all the tests that verify the functions
+ *       that are defined in the john-data.js.
+ * @see {@link https://github.com/alu0101233499/Babel-Tutorial.git}
+ *
+ */
+
+import {JSON_OBJECT, showData} from './john-data.js';
+
+const DATA = 'firstName: John\nlastName: Johnson\nnickname: Json\n' +
+  'age: 29\njob: musician\ninstrument: violin\n';
+
+describe('Tests of john-data.js', () => {
+  test('Tests for showData', () => {
+    expect(showData(JSON_OBJECT)).toEqual(DATA);
+  });
+});
